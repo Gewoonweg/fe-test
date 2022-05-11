9407cf878e4e3c91d0214d35d23cbaa22020491d
wouter <gewoonweg@gmail.com>
2022-05-11 14:56:14 +0200

    Make menu accessible with keypress (enter)

--- src/js/base.js
+++ src/js/base.js
@@ -25,20 +25,33 @@ window.onload = function() {
     /*
     * Maak hamburger menu aanklikbaar. Toggle active-menu class.
     * */
-    document.getElementById("menu-btn").addEventListener("click", function() {
+    document.getElementById("menu-btn").addEventListener("click", toggleMenu);
+    document.getElementById("menu-btn").addEventListener("keypress", onEnter);
+}
 
-        var activeClass = 'active-menu';
-        if (document.body.classList.contains(activeClass)) {
-            document.body.classList.remove(activeClass);
-        } else {
-            document.body.classList.add(activeClass);
-            var items = document.getElementsByClassName('menu-item');
-            if (items.length > 0) {
-                items[0].focus();
-            }
+var toggleMenu = function() {
+
+    var activeClass = 'active-menu';
+    if (document.body.classList.contains(activeClass)) {
+        document.body.classList.remove(activeClass);
+    } else {
+        document.body.classList.add(activeClass);
+        var items = document.getElementsByClassName('menu-item');
+        if (items.length > 0) {
+            items[0].focus();
         }
+    }
 
-        document.getElementById("menu-btn").blur();
+    document.getElementById("menu-btn").blur();
 
-    });
+}
+
+var onEnter = function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        toggleMenu()
+    }
 }
