68b6b11f21d0817edf0e947c3d5322ada72ec91f
wouter <gewoonweg@gmail.com>
2022-05-11 11:35:00 +0200

    Make base template responsive and add functioning menu

--- src/js/base.js
+++ src/js/base.js
@@ -2,7 +2,7 @@ import '../sass/base.scss'
 
 // Import vue and the example component
 import Vue from 'vue/dist/vue.js'
-import ExampleComponent from './components/ExampleComponent.vue'
+import ExampleComponent from './components/RegistrationFormComponent.vue'
 
 const init = () => {
     // Initialize javascript functions
@@ -19,3 +19,26 @@ const loadComponent = () => {
 }
 
 init()
+
+
+window.onload = function() {
+    /*
+    * Maak hamburger menu aanklikbaar. Toggle active-menu class.
+    * */
+    document.getElementById("menu-btn").addEventListener("click", function() {
+
+        var activeClass = 'active-menu';
+        if (document.body.classList.contains(activeClass)) {
+            document.body.classList.remove(activeClass);
+        } else {
+            document.body.classList.add(activeClass);
+            var items = document.getElementsByClassName('menu-item');
+            if (items.length > 0) {
+                items[0].focus();
+            }
+        }
+
+        document.getElementById("menu-btn").blur();
+
+    });
+}
